ab9705115b61db361be3e2b6681859532a888647
pakistan1224 <158190071+pakistan1224@users.noreply.github.com>
2024-06-26 23:04:40 +0500

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Izaz, a MERN Stack Developer based in Islamabad.
+            Hi! I&apos;m Izaz, a MERN Stack Developer based in Pakistan.
           </p>
 
           <a href="#about">
